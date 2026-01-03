8b17bd590a50dd45bc76c63d0d967af4809d30cd
Aayushman Ranjan <aayushmanranjan24@gmail.com>
2026-01-03 12:08:59 +0530

    Final

--- lib/Components/todaysTasks.dart
+++ lib/Components/todaysTasks.dart
@@ -287,7 +287,13 @@ class _TodaysTasksState extends State<TodaysTasks> {
 
   String formatTimeRange(DateTime start, DateTime end) {
     final formatter = DateFormat("hh:mm a");
-    return "${formatter.format(start)} - ${formatter.format(end)}";
+    final istOffset = const Duration(hours: 5, minutes: 30);
+
+    // Convert to UTC first, then add the IST offset
+    final startIst = start.toUtc().add(istOffset);
+    final endIst = end.toUtc().add(istOffset);
+
+    return "${formatter.format(startIst)} - ${formatter.format(endIst)}";
   }
 
   // --- PRIORITY HELPER ---
