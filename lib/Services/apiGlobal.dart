3be02c57114e54d73549d0538b31653449b4e64c
Aayushman Ranjan <aayushmanranjan24@gmail.com>
2026-06-19 16:00:38 +0530

    feat: implement TaskModel, add task UI components, configure Android build properties, and remove icons_plus dependency.

--- lib/Services/apiGlobal.dart
+++
@@ -1,6 +0,0 @@
-final GOOGLE_MAPS_API_KEY = 'AIzaSyD9Zvsc8PWOfEeMYwrt25MWiipPYePzd5E';
-final String version = "1.1.0";
-
- const String apiBaseURL = "https://internal.mnivesh.com";
-
-// const String apiBaseURL = "http://192.168.1.38:4000";
